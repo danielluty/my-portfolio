8f25471c2213ffa619e6b9e4332a319e9250673a
Daniel Luty <lutydaniel@gmail.com>
2019-09-05 11:32:10 +0200

    first header style version

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Fade in body */
+(() => {
+    document.getElementById('body').style.opacity = 1;
+
+    // Array.from(document.querySelectorAll('.header-container span')).map(part => {
+    //     part.style.letterSpacing = 0;
+    // });
+})();
+
