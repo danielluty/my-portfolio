36f4902a57be3a1fc24a95aad452eeb1a0526998
Daniel Luty <lutydaniel@gmail.com>
2019-09-13 16:42:46 +0200

    finished navigation function

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 /* Fade in body */
 (() => {
     document.getElementById('body').style.opacity = 1;
-
-    // Array.from(document.querySelectorAll('.header-container span')).map(part => {
-    //     part.style.letterSpacing = 0;
-    // });
 })();
 
+
+/** Toggle nav */
+const toggler = document.querySelector('.toggler');
+const navItems = Array.from(document.querySelectorAll('.nav__item'));
+
+toggler.addEventListener('click', () => {
+    document.querySelector('.menu').classList.toggle('nav--opened');
+});
+
+navItems.forEach(el => {
+    el.addEventListener('click', () => {
+        document.querySelector('.menu').classList.toggle('nav--opened');
+    })
+});
+
+/** Smooth scrolling */
